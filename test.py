a00c9edda39b24502968641cc7d6a08179edd606
Timbama <timbama@gmail.com>
2018-04-04 10:27:23 -0400

    Merge branch 'master' of https://github.com/Timbama/HSI_Dictionaries

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import numpy as np
 from USG_data_paths import dataPath_HSI
 import matplotlib.pyplot as plt
 from extract_data import get_spectral_library, create_sample, normalize, convert_library
-from create_hsi import create_hsi
+from create_hsi import create_hsi, abundance_map
 from sklearn.decomposition import sparse_encode
 from sklearn.preprocessing import Imputer
 library = get_spectral_library('AVIRIS2014','Minerals')
@@ -16,8 +16,9 @@ print(np.nanmin(samples))
 print(len(library))
 
 image = create_hsi(samples)
+image_test = create_hsi(samples)
 print(image.shape)
-
+abundance = abundance_map((.5, .33333, .25, .2), 1, (75,75))
 data = np.reshape(image, (image.shape[0]*image.shape[1], image.shape[2]))
 print(data.shape)
 
@@ -29,7 +30,7 @@ imputer_data.fit(data)
 imputer_dict.fit(dictionary)
 data = imputer_data.transform(data)
 dictionary = imputer_dict.transform(dictionary)
-sparse = sparse_encode(data, dictionary, algorithm='lasso_lars', max_iter=200000, n_nonzero_coefs=20, alpha=2)
+sparse = sparse_encode(data, dictionary, algorithm='lasso_cd', max_iter=1000, n_nonzero_coefs=20, alpha=2)
 
 print(sparse.shape)
 
@@ -55,4 +56,6 @@ print(used)
 #for i in range(5): 
     #print(np.sum(image[:,:,i]))
 plt.imshow(image[:,:])
+plt.imshow(image_test[:,:,20])
+plt.imshow(abundance)
 plt.show()
